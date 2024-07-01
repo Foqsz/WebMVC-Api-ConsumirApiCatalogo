08c90b038fb42f3a136a23dad6a38bc119fecdc9
Victor Vinicius <victormafiavinicius@gmail.com>
2024-07-01 11:51:34 -0300

    Consumindo a api com atualizar categoria

--- WebApplication1/Controllers/CategoriasController.cs
+++ WebApplication1/Controllers/CategoriasController.cs
@@ -47,5 +47,34 @@ namespace CategoriasMvc.Controllers
             ViewBag.Erro = "Erro ao criar uma categoria";
             return View(categoriaVM);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarCategoria(int id)
+        {
+            var result = await _categoriaService.GetCategoriaPorId(id);
+
+            if (result is null)
+            {
+                return View("Error");
+            }
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoriaViewModel>> AtualizarCategoria(int id, CategoriaViewModel categoriaVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _categoriaService.AtualizarCategoria(id, categoriaVM);
+                if (result)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                
+            }
+            ViewBag.Erro = "Erro ao atualizar Categoria";
+            return View(categoriaVM);
+        }
+
     }
 }
